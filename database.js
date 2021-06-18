4ea0528f8a4fd88202ba0912b67c77868e4e71c5
silvanaamaralpe@gmail.com <amaral>
2021-06-17 21:37:03 -0300

    adicionando o meu banco de dados

---
+++ database.js
@@ -0,0 +1,26 @@
+const leituraDeLivros = [
+    {
+        nome:'O poder do hábito',
+        categoria:'Autoajuda',
+        paginas:408,
+        recomenda:true,
+        leu:false,
+    },
+    {
+        nome:'Sem lugar para se esconder',
+        categoria:'Nao ficcao',
+        paginas:285,
+        recomenda:true,
+        leu:true,
+
+    },
+    {
+        nome:'Homens, ideias e acoes politicas',
+        categoria:'Politica',
+        paginas:263,
+        recomenda:true,
+        leu:false,
+    },
+
+]
+module.exports = leituraDeLivros
